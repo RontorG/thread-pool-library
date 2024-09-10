54d26abe252083797d4c03467c7255c9766d771c
Lucas Pereira <lucasspp2003@outlook.com>
2024-09-10 00:04:22 -0300

    fixed structures

--- thread_pool.h
+++ thread_pool.h
@@ -12,28 +12,29 @@ typedef struct __tpool_task_t {
 
 typedef struct __tpool_task_queue_t {
 	int ntasks;
-	struct __tpool_task_t *head;
-	struct __tpool_task_t *tail;
+	struct __tpool_task_t task;
 } tpool_task_queue_t;
 
 
 typedef struct __tpool_worker_t {
 	pthread_t tid;
-	struct __tpool_worker_t *next;
 	struct __tpool_task_t task;
 } tpool_worker_t;
 
 
 typedef struct __tpool_t {
+	int head;
+	int tail;
 	int nworkers;
-	struct __tpool_worker_t *head;
-	struct __tpool_worker_t *tail;
-	pthread_mutex_t mutex;
 	pthread_cond_t cond;
+	pthread_mutex_t mutex;
+	struct __tpool_worker_t *workers;
+	struct __tpool_task_queue_t tasks;
 } tpool_t;
 
 
-int tpool_init(size_t nthreads);
+int tpool_init(tpool_t *tp, size_t nthreads);
+void *tpool_worker_wait();
 int tpool_add_task(tpool_task_t task);
 
 #endif
