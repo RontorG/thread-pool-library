675a21870c48bac541129b0b05d98ed9cc15341c
Lucas Pereira <lucasspp2003@outlook.com>
2024-09-29 01:53:17 -0300

    add tpool_worker_instance()

--- src/thread_pool.c
+++ src/thread_pool.c
@@ -10,7 +10,7 @@ int tpool_init(tpool_t *tp, int nthreads, int ntasks)
 	tp->workers = malloc(sizeof(*tp->workers) * nthreads);
 
 	for (int i = 0; i < nthreads; i++) {
-		if (pthread_create(&tp->workers[i].tid, NULL, tpool_worker_wait, NULL) != 0) {
+		if (pthread_create(&tp->workers[i].tid, NULL, (void *)tpool_worker_instance, NULL) != 0) {
 			fprintf(stderr, "Failed to create threads\n");
 			exit(EXIT_FAILURE);
 		}
@@ -53,4 +53,28 @@ int tpool_task_isfull(tpool_t *tp)
 int tpool_task_isempty(tpool_t *tp)
 {
 	return tp->tasks.head == tp->tasks.tail;
+}
+
+
+void *tpool_worker_instance(tpool_t *tp)
+{
+	int tindex;
+
+	while (1) {
+		pthread_mutex_lock(&tp->mutex);
+		
+		// Is there any task to do?
+		while (tpool_task_isempty(tp)) { 
+			// No: wait for new tasks
+			pthread_cond_wait(&tp->cond, &tp->mutex);
+		}
+
+		// Yes: decrease todo, do the task
+		tp->tasks.todo--;
+		tindex = tp->tasks.head;
+		pthread_mutex_unlock(&tp->mutex);
+
+		// Ugly function call
+		tp->tasks.task[tindex].function(tp->tasks.task[tindex].arg);
+	}
 }
